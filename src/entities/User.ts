a27ce8110001a462250155b0d64050cb6947c0bd
mason-bz <mason.sim.bz@gmail.com>
2020-06-23 22:02:27 +0900

    hashing User Passwords

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,14 +1,17 @@
+import bcrypt from "bcrypt";
 import { IsEmail } from "class-validator";
 
 import {
   BaseEntity,
+  BeforeInsert,
+  BeforeUpdate,
   Column,
   CreateDateColumn,
   Entity,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from "typeorm";
-
+const BCRYPT_ROUNDS = 10;
 @Entity()
 class User extends BaseEntity {
   @PrimaryGeneratedColumn() id: number;
@@ -65,6 +68,19 @@ class User extends BaseEntity {
 
   @CreateDateColumn() createAt: string;
   @UpdateDateColumn() updateAt: string;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async savePassword(): Promise<void> {
+    if (this.password) {
+      const hashedPassword = await this.hashPassword(this.password);
+      this.password = hashedPassword;
+    }
+  }
+
+  private hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, BCRYPT_ROUNDS);
+  }
 }
 
 export default User;
